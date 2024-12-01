8b74c66945fc39dad2d138d1f7b84fb00eb6f168
Carlos Contreras Sanz <contrerassanz.carlos@gmail.com>
2024-12-01 11:46:22 +0100

    AOC 2024 - Day 1

---
+++ src/main/kotlin/es/carloscsanz/adventofcode/Day1HistorianHysteria.kt
@@ -0,0 +1,56 @@
+package es.carloscsanz.adventofcode
+
+import kotlin.math.absoluteValue
+
+fun main() {
+    val exercise = Day1HistorianHysteria("/day-1/input.txt")
+
+    val firstChallenge = exercise.firstChallenge()
+    val secondChallenge = exercise.secondChallenge()
+
+    println("DAY 1:")
+    println("- [CHALLENGE 1] Total distance: $firstChallenge")
+    println("- [CHALLENGE 2] Similarity score: $secondChallenge")
+}
+
+class Day1HistorianHysteria(filePath: String) {
+    private val leftList: List<Long>
+    private val rightList: List<Long>
+
+    init {
+        val left = mutableListOf<Long>()
+        val right = mutableListOf<Long>()
+
+        this::class.java.getResourceAsStream(filePath)!!
+            .bufferedReader()
+            .forEachLine { line ->
+                val (rightNumber, leftNumber) = line.split("   ")
+
+                left.add(leftNumber.toLong())
+                right.add(rightNumber.toLong())
+            }
+
+        leftList = left
+        rightList = right
+    }
+
+    fun firstChallenge(): Long {
+        val rightSorted = rightList.sorted()
+        val distances = leftList.sorted().mapIndexed { index, value ->
+            val otherValue = rightSorted[index]
+            (value - otherValue).absoluteValue
+        }
+
+        return distances.sum()
+    }
+
+    fun secondChallenge(): Long {
+        val values = rightList.groupBy { it }
+        val scores = leftList.sorted().map { value ->
+            val occurrences = values[value]?.count() ?: 0
+            value * occurrences
+        }
+
+        return scores.sum()
+    }
+}
---
+++ src/test/kotlin/es/carloscsanz/adventofcode/Day1HistorianHysteriaTest.kt
@@ -0,0 +1,22 @@
+package es.carloscsanz.adventofcode
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day1HistorianHysteriaTest {
+    private val exercise = Day1HistorianHysteria("/day-1/input.txt")
+
+    @Test
+    fun `should calculate the expected total distance for the first challenge`() {
+        val actual = exercise.firstChallenge()
+
+        actual shouldBe 11
+    }
+
+    @Test
+    fun `should calculate the expected similarity score for the second challenge`() {
+        val actual = exercise.secondChallenge()
+
+        actual shouldBe 31
+    }
+ }
